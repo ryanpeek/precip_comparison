792d5ce59fd966371d3f1af321dd60fdd664eee5
Ryan Peek <rapeek@ucdavis.edu>
2016-03-07 23:41:44 -0800

    update scripts

--- scripts/ucd_climate.R
+++ scripts/ucd_climate.R
@@ -6,7 +6,8 @@
 library(lubridate); library(dplyr); library(ggplot2); library(stringr)
 
 # get historical climate data
-dav <- read.csv("./data/climate_Davis_historical.csv", skip = 53) %>% 
+dav <- read.csv("./data/climate_Davis_historical.csv", skip = 53)
+dav %<>% 
   select(Station:min,Soil.max, min.1) %>% 
   filter(!is.na(Air.max))
 
@@ -22,6 +23,7 @@ dav1 <- dav %>%
   select(datetime, Station, Precip:min.1, Y, M, DOY) %>% 
   group_by(Y, M, Station) %>% 
   filter(M==2) # limit to FEB 
+glimpse(dav1)
   
 dav2 <- dav %>% 
   select(datetime, Station, Precip:min.1, Y, M, DOY) %>% 
@@ -29,12 +31,46 @@ dav2 <- dav %>%
   filter(M==2) %>% # limit to FEB 
   summarize("totPPT_mm"=sum(Precip),
             "avgAir.max"=mean(Air.max))
-glimpse(dav1)
+
+ppt <- dav %>% 
+  select(datetime, Precip:min.1, Y, M, DOY) %>% 
+  group_by(Y, M) %>% 
+  filter(M==2) %>% # limit to FEB 
+  summarize("totPPT_mm"=sum(Precip),
+            "avgPPT_mm"=mean(Precip),
+            "maxPPT_mm"=max(Precip),
+            "minPPT_mm"=min(Precip))
+
+glimpse(ppt)
 
 # precip in FEB
-ggplot(data = dav2, aes(x = Y, y = totPPT, group=Station)) +
-  geom_point(data= dav2, aes(x=Y, y=totPPT, group=Station), alpha= 0.5, color="red2")+
-  geom_point(data=dav2[dav2$Y==2016,], aes(x=Y, y=totPPT, group=Station), fill="blue", alpha=0.7) + theme_bw() #+ facet_grid(.~M)
+ggplot(data = ppt, aes(x = as.factor(Y), y = avgPPT_mm, group=Y)) +
+  geom_crossbar(data= ppt, aes(x=as.factor(Y), ymax=maxPPT_mm, ymin=minPPT_mm, group=as.factor(Y)), alpha= 0.5, color="red2")+
+  #geom_point(data=ppt[ppt$Y==2016,], aes(x=as.factor(Y), y=maxPPT_mm, group=as.factor(Y)), fill="blue", alpha=0.7) + theme_bw() +
+  facet_grid(M~.)
+
+# precip plot
+ggplot(data = ppt, aes(x = as.factor(Y), y = totPPT_mm, group=Y)) +
+  geom_point(data=ppt[ppt$Y==2016,], aes(x=as.factor(Y), y=totPPT_mm, group=Y), color="maroon", alpha=0.8) +
+  geom_smooth(data=ppt, aes(x=as.factor(Y), y= totPPT_mm, group=M), alpha=0.3) +
+  geom_point(data=ppt, aes(x=as.factor(Y),y=totPPT_mm, group=Y), color="gray20", size=2.7)+
+  geom_point(data=ppt[ppt$Y==2016,], aes(x=as.factor(Y),y=totPPT_mm, group=Y), pch=21, fill="maroon", size=4) +
+  geom_point(data=ppt[ppt$Y==1983,], aes(x=as.factor(Y),y=totPPT_mm, group=Y), pch=21, fill="maroon", size=4) +
+  geom_point(data=ppt[ppt$Y==1998,], aes(x=as.factor(Y),y=totPPT_mm, group=Y), pch=21, fill="maroon", size=4) +
+  geom_point(data=ppt[ppt$Y==1988,], aes(x=as.factor(Y),y=totPPT_mm, group=Y), pch=21, fill="lightpink", size=4) +
+  geom_point(data=ppt[ppt$Y==1992,], aes(x=as.factor(Y),y=totPPT_mm, group=Y), pch=21, fill="lightpink", size=4) +
+  geom_point(data=ppt[ppt$Y==2003,], aes(x=as.factor(Y),y=totPPT_mm, group=Y), pch=21, fill="lightpink", size=4) +
+  geom_point(data=ppt[ppt$Y==2010,], aes(x=as.factor(Y),y=totPPT_mm, group=Y), pch=21, fill="lightpink", size=4) +
+  geom_text(data=ppt[ppt$Y==2016 | ppt$Y==1983 | ppt$Y==1988 | ppt$Y==1998 | ppt$Y==1992 | ppt$Y==2003 | ppt$Y==2010,], 
+             aes(label=Y, x=as.factor(Y), y=totPPT_mm), size=3, vjust = -0.90, nudge_y=-0.5, fontface = "bold")+
+  ylab(paste("Total Precipitation (mm)")) + theme_bw() + xlab("") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) + 
+  ggtitle(label = "February Precip (mm) in Davis: 1981-2016") + 
+  annotate("text", x=as.factor(2008), y=290, size=2, label="Data Source: http://atm.ucdavis.edu/weather/")
+
+library(svglite) # much higher res and is vector scalable graphics
+ggsave(filename = "./plots/Feb_ppt_Davis_1981-2016.svg", width = 8, height=5, units = "in", dpi = 150)
+
 
 # airtemp in FEB
 ggplot(data = dav1, aes(x = as.factor(Y), y = Air.max, group=Station)) +
@@ -62,7 +98,6 @@ ggsave(filename = "./plots/Feb_airtemp_Davis_1981-2016.svg", width = 8, height=5
 ggplot(data = dav2, aes(x = Y, y = avgAir.max, group=Station)) +
   geom_jitter(alpha = 0.3, color = "blue3") +
   geom_boxplot(fill="blue", alpha=0.7) + theme_bw()
-h(dav2)
 
 
 dav %>% 
